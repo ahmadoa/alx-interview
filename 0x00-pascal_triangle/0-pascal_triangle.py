a67273d393504713c18779c70008e9ee6af1370d
ahmadoa <ouladaouida@gmail.com>
2023-11-29 23:02:10 +0000

    pascal's triangle algorithm

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,22 @@
+#!/usr/bin/python3
+"""0-pascal_triangle"""
+
+
 def pascal_triangle(n):
-    res = []
+    """pascal triange
+    Args:
+        n (integer): number of rows
+
+    Returns:
+        list: pascal triangle
+    """
+    res = [[1]]
     if n <= 0:
-        return res
-    for i in range(n):
-        if len(res) === 0:
-            res.append([1])
-        else:
-            row = [1]
-            for j in range(1, len(res[-1])):
-                row.append(res[-1][j] + res[-1][j - 1])
-            row.append(1)
-            res.append(row)
+        return []
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
     return res
